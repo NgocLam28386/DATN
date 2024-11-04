c8dfb18d29258543314ec0d954487204d7dc5019
luuvankhanh2 <khanhlvph41483@fpt.edu.vn>
2024-11-05 00:27:49 +0700

    typeshipController

--- src/controller/typeshipController.js
+++ src/controller/typeshipController.js
@@ -1,1 +1,17 @@
 import typeshipService from '../services/typeshipService';
+
+let createNewTypeShip = async (req, res) => {
+    try {
+        let data = await typeshipService.createNewTypeShip(req.body);
+        return res.status(200).json(data);
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
+module.exports = {
+    createNewTypeShip: createNewTypeShip,
+}
