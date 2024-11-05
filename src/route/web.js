b58ac9eabc84f24ea5c6614720cef9223db244da
ngocquang04 <quangkhong09@gmail.com>
2024-11-05 07:31:38 +0700

    route cart

--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import receiptController from '../controllers/receiptController';
+import shopCartController from '../controllers/shopCartController';
 let router = express.Router();
 
 let initwebRoutes = (app) => {
@@ -13,6 +14,11 @@ let initwebRoutes = (app) => {
      router.put('/api/update-receipt', middlewareControllers.verifyTokenAdmin, receiptController.updateReceipt)
      router.delete('/api/delete-receipt', middlewareControllers.verifyTokenAdmin, receiptController.deleteReceipt)
      router.post('/api/create-new-detail-receipt', middlewareControllers.verifyTokenAdmin, receiptController.createNewReceiptDetail)
+
+       //=================API SHOPCART==========================//
+    router.post('/api/add-shopcart', middlewareControllers.verifyTokenUser, shopCartController.addShopCart)
+    router.get('/api/get-all-shopcart-by-userId', middlewareControllers.verifyTokenUser, shopCartController.getAllShopCartByUserId)
+    router.delete('/api/delete-item-shopcart', middlewareControllers.verifyTokenUser, shopCartController.deleteItemShopCart)
     return app.use("/", router);
 }
 
