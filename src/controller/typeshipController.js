72b2b5ccc54bb390117624fdd3ca581194adb907
luuvankhanh2 <khanhlvph41483@fpt.edu.vn>
2024-11-05 10:48:44 +0700

    typeshipController

--- src/controller/typeshipController.js
+++ src/controller/typeshipController.js
@@ -24,7 +24,20 @@ let getDetailTypeshipById = async (req, res) => {
         })
     }
 }
+let getAllTypeship = async (req, res) => {
+    try {
+        let data = await typeshipService.getAllTypeship(req.query);
+        return res.status(200).json(data);
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
 module.exports = {
     createNewTypeShip: createNewTypeShip,
-    getDetailTypeshipById: getDetailTypeshipById
+    getDetailTypeshipById: getDetailTypeshipById,
+    getAllTypeship: getAllTypeship
 }
