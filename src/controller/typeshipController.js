6159385a686f5daf5fb4ceba6a2402cd6f97997f
luuvankhanh2 <khanhlvph41483@fpt.edu.vn>
2024-11-05 10:52:24 +0700

    typeshipController

--- src/controller/typeshipController.js
+++ src/controller/typeshipController.js
@@ -48,9 +48,22 @@ let updateTypeship = async (req, res) => {
         })
     }
 }
+let deleteTypeship = async (req, res) => {
+    try {
+        let data = await typeshipService.deleteTypeship(req.body);
+        return res.status(200).json(data);
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
 module.exports = {
     createNewTypeShip: createNewTypeShip,
     getDetailTypeshipById: getDetailTypeshipById,
     getAllTypeship: getAllTypeship,
-    updateTypeship: updateTypeship
+    updateTypeship: updateTypeship,
+    deleteTypeship: deleteTypeship
 }
