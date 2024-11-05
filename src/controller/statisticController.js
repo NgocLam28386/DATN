6d08d930cbb57229f81fd9f4eaf5dcefd61dd1c7
ngocquang04 <quangkhong09@gmail.com>
2024-11-05 20:54:09 +0700

    Đếm số thẻ thống kê, Đếm trạng thái đơn hàng

---
+++ src/controller/statisticController.js
@@ -0,0 +1,25 @@
+import statisticService from '../services/statisticService';
+let getCountCardStatistic = async (req, res) => {
+    try {
+        let data = await statisticService.getCountCardStatistic(req.query);
+        return res.status(200).json(data);
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
+let getCountStatusOrder = async (req, res) => {
+    try {
+        let data = await statisticService.getCountStatusOrder(req.query);
+        return res.status(200).json(data);
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
