f09c827a3c1d2b5bf0a361801a191d7d485c6128
HuyenLK19 <lkhuyen.mmiv@gmail.com>
2024-11-07 00:04:00 +0700

    update mig address

--- src/controller/commentController.js
+++ src/controller/commentController.js
@@ -25,7 +25,19 @@ let getAllReviewByProductId = async (req, res) => {
         })
     }
 }
+let ReplyReview = async (req, res) => {
+    try {
 
+        let data = await commentService.ReplyReview(req.body);
+        return res.status(200).json(data);
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
 let deleteReview = async (req, res) => {
     try {
 
