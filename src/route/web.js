a9e0ffa555ff186a939ab5b68b0f48bd42d0baaf
ngocquang04 <quangkhong09@gmail.com>
2024-11-06 21:04:16 +0700

    route thống kê ngày

--- src/route/web.js
+++ src/route/web.js
@@ -25,6 +25,9 @@ let initwebRoutes = (app) => {
     router.get('/api/get-count-card-statistic', middlewareControllers.verifyTokenAdmin, statisticController.getCountCardStatistic)
     router.get('/api/get-count-status-order', middlewareControllers.verifyTokenAdmin, statisticController.getCountStatusOrder)
     router.get('/api/get-statistic-by-month', middlewareControllers.verifyTokenAdmin, statisticController.getStatisticByMonth)
+    router.get('/api/get-statistic-by-day', middlewareControllers.verifyTokenAdmin, statisticController.getStatisticByDay)
+
+    
     return app.use("/", router);
 
       
