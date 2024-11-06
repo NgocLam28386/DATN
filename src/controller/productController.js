9317b080c0bdba7ba22862c7484389a5a15509d8
Đinh Ngọc Lâm <dinhngoclam28386@gmail.com>
2024-11-06 21:46:49 +0700

    Chuc năng cua phan product CRUD

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -1,1 +1,31 @@
 import productService from '../services/productService';
+
+let createNewProduct = async (req, res) => {
+    try {
+        let data = await productService.createNewProduct(req.body);
+        return res.status(200).json(data);
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
+let getAllProductAdmin = async (req, res) => {
+    try {
+        let data = await productService.getAllProductAdmin(req.query);
+        return res.status(200).json(data);
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
+
+module.exports = {
+    createNewProduct: createNewProduct,
+    getAllProductAdmin: getAllProductAdmin,
+}
