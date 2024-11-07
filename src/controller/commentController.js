fb95f3226c0691fadf5c5bf4d236ac4a2719212c
Đinh Ngọc Lâm <dinhngoclam28386@gmail.com>
2024-11-07 23:35:35 +0700

    createNewComment

--- src/controller/commentController.js
+++ src/controller/commentController.js
@@ -51,10 +51,24 @@ let deleteReview = async (req, res) => {
         })
     }
 }
+let createNewComment = async (req, res) => {
+    try {
+        let data = await commentService.createNewComment(req.body);
+        return res.status(200).json(data);
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
 
 module.exports = {
     createNewReview: createNewReview,
     getAllReviewByProductId: getAllReviewByProductId,
     ReplyReview: ReplyReview,
     deleteReview: deleteReview,
+    createNewComment:createNewComment,
+
 }
