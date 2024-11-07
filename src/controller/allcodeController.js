2af33ce374636add1b16d3622f18a6555fea929c
HuyenLK19 <lkhuyen.mmiv@gmail.com>
2024-11-07 23:38:43 +0700

    load message fix

--- src/controller/allcodeController.js
+++ src/controller/allcodeController.js
@@ -24,7 +24,18 @@ let getAllCodeService = async (req, res) => {
         })
     }
 }
-
+let getAllCategoryBlog = async (req, res) => {
+    try {
+        let data = await allcodeService.getAllCategoryBlog(req.query.type);
+        return res.status(200).json(data);
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
 let handleUpdateAllCode = async (req, res) => {
     try {
         let data = await allcodeService.handleUpdateAllCode(req.body);
