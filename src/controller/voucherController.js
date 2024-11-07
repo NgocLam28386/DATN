0667894ba03a4738c5c5d26d46b03ff398605215
luuvankhanh2 <khanhlvph41483@fpt.edu.vn>
2024-11-07 13:56:58 +0700

    getAllTypeVoucher

--- src/controller/voucherController.js
+++ src/controller/voucherController.js
@@ -22,7 +22,20 @@ let getDetailTypeVoucherById = async (req, res) => {
         })
     }
 }
+let getAllTypeVoucher = async (req, res) => {
+    try {
+        let data = await voucherService.getAllTypeVoucher(req.query);
+        return res.status(200).json(data);
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
 module.exports = {
     createNewTypeVoucher: createNewTypeVoucher,
-    getDetailTypeVoucherById: getDetailTypeVoucherById
+    getDetailTypeVoucherById: getDetailTypeVoucherById,
+    getAllTypeVoucher: getAllTypeVoucher
 }
