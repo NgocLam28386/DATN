46a51e544ba0f161565a9ac1134626c7b2da37bc
HuyenLK19 <lkhuyen.mmiv@gmail.com>
2024-11-07 23:40:15 +0700

    delete type code

--- src/controller/messageController.js
+++ src/controller/messageController.js
@@ -48,6 +48,18 @@ let listRoomOfUser = async (req, res) => {
         })
     }
 }
+let listRoomOfAdmin = async (req, res) => {
+    try {
+        let data = await messageService.listRoomOfAdmin();
+        return res.status(200).json(data);
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
 
 module.exports = {
     createNewRoom: createNewRoom,
