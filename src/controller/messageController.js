4c9140e819bc894f6162362d02848985d0d3f984
HuyenLK19 <lkhuyen.mmiv@gmail.com>
2024-11-07 23:39:25 +0700

    admin dashboard complete

--- src/controller/messageController.js
+++ src/controller/messageController.js
@@ -48,18 +48,6 @@ let listRoomOfUser = async (req, res) => {
         })
     }
 }
-let listRoomOfAdmin = async (req, res) => {
-    try {
-        let data = await messageService.listRoomOfAdmin();
-        return res.status(200).json(data);
-    } catch (error) {
-        console.log(error)
-        return res.status(200).json({
-            errCode: -1,
-            errMessage: 'Error from server'
-        })
-    }
-}
 
 module.exports = {
     createNewRoom: createNewRoom,
