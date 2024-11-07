b1a26c6d09f74db388e4d01b3df6cdae4f14792d
luuvankhanh2 <khanhlvph41483@fpt.edu.vn>
2024-11-07 07:09:11 +0700

    createNewTypeVoucher

--- src/controller/voucherController.js
+++ src/controller/voucherController.js
@@ -1,1 +1,15 @@
-import voucherService from '../services/voucherService';
+import voucherService from '../services/voucherService';let createNewTypeVoucher = async (req, res) => {
+    try {
+        let data = await voucherService.createNewTypeVoucher(req.body);
+        return res.status(200).json(data);
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
+module.exports = {
+    createNewTypeVoucher: createNewTypeVoucher
+}
