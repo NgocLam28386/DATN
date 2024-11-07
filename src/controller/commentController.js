177167ea5a3e321ebfedc74443960bc51cfbac1f
Đinh Ngọc Lâm <dinhngoclam28386@gmail.com>
2024-11-08 00:30:00 +0700

    ReplyComment

--- src/controller/commentController.js
+++ src/controller/commentController.js
@@ -76,6 +76,19 @@ let getAllCommentByBlogId = async (req, res) => {
         })
     }
 }
+let ReplyComment = async (req, res) => {
+    try {
+
+        let data = await commentService.ReplyComment(req.body);
+        return res.status(200).json(data);
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
 
 module.exports = {
     createNewReview: createNewReview,
@@ -84,5 +97,7 @@ module.exports = {
     deleteReview: deleteReview,
     createNewComment:createNewComment,
     getAllCommentByBlogId:getAllCommentByBlogId,
+    ReplyComment:ReplyComment,
+
 
 }
