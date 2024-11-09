0267ffce0331ec933696c52aae4f3f8063f2d507
ngocquang04 <quangkhong09@gmail.com>
2024-11-09 07:04:55 +0700

    getAllSupplier + getDetailSupplierById service

--- src/services/supplierService.js
+++ src/services/supplierService.js
@@ -28,6 +28,54 @@ let createNewSupplier = (data) => {
         }
     })
 }
+let getDetailSupplierById = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!id) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameter !'
+                })
+            } else {
+               
+                let res = await db.Supplier.findOne({
+                    where: { id: id }
+                  
+                })
+              
+             
+                resolve({
+                    errCode: 0,
+                    data: res
+                })
+            }
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+let getAllSupplier = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let objectFilter = {}
+            if (data.limit && data.offset) {
+                objectFilter.limit = +data.limit
+                objectFilter.offset = +data.offset
+            }
+            if(data.keyword !=='') objectFilter.where = {...objectFilter.where, name: {[Op.substring]: data.keyword  } }
+            let res = await db.Supplier.findAndCountAll(objectFilter)    
+            resolve({
+                errCode: 0,
+                data: res.rows,
+                count: res.count
+            })
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
 module.exports = {
     createNewSupplier: createNewSupplier,
+    getDetailSupplierById:getDetailSupplierById,
+    getAllSupplier:getAllSupplier,
 }
