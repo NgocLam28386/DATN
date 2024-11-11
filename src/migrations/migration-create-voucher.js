f8022215e28907d323c72fee6536bf7534a38a3b
luuvankhanh2 <khanhlvph41483@fpt.edu.vn>
2024-11-11 12:28:37 +0700

    migration-create-voucher

--- src/migrations/migration-create-voucher.js
+++ src/migrations/migration-create-voucher.js
@@ -22,6 +22,14 @@ module.exports = {
             },
             amount: {
                 type: Sequelize.INTEGER
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
             }
         });
     },
