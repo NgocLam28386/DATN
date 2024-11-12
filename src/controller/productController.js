c30a55c952b195ae63af9dc7b7053413637a5aae
Đinh Ngọc Lâm <dinhngoclam28386@gmail.com>
2024-11-12 20:26:48 +0700

    updateProductDetailImage

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -168,6 +168,18 @@ let getDetailProductImageById = async (req, res) => {
         })
     }
 }
+let updateProductDetailImage = async (req, res) => {
+    try {
+        let data = await productService.updateProductDetailImage(req.body);
+        return res.status(200).json(data);
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
 
 module.exports = {
     createNewProduct: createNewProduct,
@@ -184,6 +196,7 @@ module.exports = {
     getDetailProductDetailById: getDetailProductDetailById,
     createNewProductDetailImage: createNewProductDetailImage,
     getDetailProductImageById: getDetailProductImageById,
+    updateProductDetailImage: updateProductDetailImage,
 
 
 }
